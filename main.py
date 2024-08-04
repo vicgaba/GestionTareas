f877c606d6d9af378d623f33f2fcffabe09e0cce
Victor Gabardini <victorgabardini@gmail.com>
2024-08-04 12:52:42 -0300

    Creada clase tareas y main

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+import os
+
+from tareas import(
+    GestionTareas,
+    Tarea
+)
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def mostrar_menu():
+    print("1. Agregar Tarea")
+    print("2. Mostrar Tareas")
+    print("3. Salir")
+    print('======================================================')
+def agregar_tarea(gestion):
+    id = input("Ingrese el ID de la tarea: ")
+    titulo = input("Ingrese el título de la tarea: ")
+    descripcion = input("Ingrese la descripción de la tarea: ")
+    fecha_ingreso = input("Ingrese la fecha de ingreso (YYYY-MM-DD): ")
+    fecha_vencimiento = input("Ingrese la fecha de vencimiento (YYYY-MM-DD): ")
+    estado = input("Ingese el estado, 1. Pendiente, 2. En Progreso, 3. Completada")
+    tarea = Tarea(id, titulo, descripcion, fecha_ingreso, fecha_vencimiento, estado)
+
+    gestion.agregar_tarea(tarea)
+    print("Tarea agregada correctamente.")
+    input("Presione Enter para continuar...")
+    
+if __name__ == '__main__':
+    archivo_tareas = 'tareas.json'
+    gestion = GestionTareas(archivo_tareas)
+    limpiar_pantalla()
+    mostrar_menu()
+    while True:
+        limpiar_pantalla()
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+        if opcion == '1':
+            agregar_tarea(gestion)
+        elif opcion == '2':
+            gestion.mostrar_tareas()
+            input("Presione Enter para continuar...")
+        elif opcion == '3':
+            sys.exit()
---
+++ tareas.py
@@ -0,0 +1,124 @@
+import datetime
+import json
+
+class Tarea:
+    def __init__(self, id, titulo, descripcion, fecha_ingreso, fecha_vencimiento, estado):
+        self.id = self.validar_id(id)
+        self.titulo = self.validar_titulo(titulo)
+        self.descripcion = descripcion
+        self.fecha_ingreso = fecha_ingreso
+        self.fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
+        self.estado = self.validar_estado(estado)
+
+    def __str__(self):
+        return f"{self.descripcion} - Ingreso: {self.fecha_ingreso} - Vencimiento: {self.fecha_vencimiento} - Estado: {self.estado}"
+    
+    @property
+    def id(self):
+        return self.__id
+    @property
+    def titulo(self):
+        return self.__titulo
+    @property
+    def description(self):
+        return self.__descripcion
+    @property
+    def fecha_ingreso(self):
+        return self.__fecha_ingreso
+    @property
+    def fecha_vencimiento(self):
+        return self.__fecha_vencimiento
+    @property
+    def estado(self):
+        return self.__estado
+    @fecha_ingreso.setter
+    def fecha_ingreso(self, fecha_ingreso):
+        self.__fecha_ingreso = fecha_ingreso
+    @fecha_vencimiento.setter
+    def fecha_vencimiento(self, fecha_vencimiento):
+        self.__fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
+        return self.__fecha_vencimiento
+    @estado.setter
+    def estado(self, estado):
+        self.__estado = self.validar_estado(estado)
+
+    @id.setter
+    def id(self, id):
+        self.__id = self.validar_id(id)
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self.__titulo = self.validar_titulo(titulo)
+        return self.__titulo
+    
+    def validar_titulo(self, titulo):
+        if not isinstance(titulo, str) or not titulo.strip():
+            raise ValueError("El título no puede estar vacío.")
+        return titulo
+    
+    def validar_id(self, id):
+        #ToDo validar que el id no exista para los nuevos y si exista para update y delete
+        if not isinstance(int(id), int) or int(id) <= 0:
+            raise ValueError("El ID debe ser un número entero positivo.")
+        return id
+
+    def validar_fecha_vencimiento(self, fecha_vencimiento):
+        try:
+            fecha_vencimiento = datetime.date(fecha_vencimiento)
+            fecha_actual = datetime.date.today()
+            if fecha_vencimiento.date() < fecha_actual:
+                raise ValueError("La fecha de vencimiento no puede ser en el pasado.")
+        except ValueError:
+            raise ValueError("Formato de fecha inválido. Debe ser YYYY-MM-DD.")
+        return fecha_vencimiento
+    
+    def validar_estado(estado):
+        if estado not in ["pendiente", "en progreso", "completada"]:
+            raise ValueError("Estado inválido. Debe ser 'pendiente', 'en progreso' o 'completada'.")
+        return estado
+    
+    def to_dict(self):
+        return {
+            "descripcion": self.__descripcion,
+            "fecha_ingreso": self.__fecha_ingreso,
+            "fecha_vencimiento": self.__fecha_vencimiento,
+            "estado": self.__estado
+        }
+
+class GestionTareas:
+    def __init__(self, archivo):
+        self.archivo = archivo
+
+    def leer_datos(self):
+        try:
+            with open(self.archivo, 'r') as file:
+                datos = json.load(file)
+        except FileNotFoundError:
+            datos = []
+        return datos
+    def agregar_tarea(self, tarea):
+        datos = self.leer_datos()
+        datos.append(tarea.to_dict())
+        self.guardar_datos(datos)
+        print("Tarea agregada correctamente.")
+
+    def guardar_datos(self, datos):
+        with open(self.archivo, 'w') as file:
+            json.dump(datos, file, indent=4)
+    def mostrar_tareas(self):
+        datos = self.leer_datos()
+        if not datos:
+            print("No hay tareas registradas.")
+        else:
+            for tarea in datos:
+                print(Tareas(**tarea))
+        print("-----------------------------------------------------------------------------")
+
+    def eliminar_tarea(self, descripcion):
+        datos = self.leer_datos()
+        datos_filtrados = [tarea for tarea in datos if tarea["descripcion"] != descripcion]
+        if len(datos_filtrados) == len(datos):
+            print(f"No se encontró una tarea con la descripción '{descripcion}'.")
+        else:
+            self.guardar_datos(datos_filtrados)
+            print(f"Tarea con descripción '{descripcion}' eliminada correctamente.")
