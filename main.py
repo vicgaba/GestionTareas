e64c6f1df443d51f7e914712ba33033c08c8da7d
Victor Gabardini <victorgabardini@gmail.com>
2024-08-05 20:49:28 -0300

    Creadas las 2 subclases y diferenciado el print para cada una

--- main.py
+++ main.py
@@ -3,24 +3,39 @@ import os
 
 from tareas import(
     GestionTareas,
-    Tarea
+    TareaSimple,
+    TareaRecurrente
 )
 def limpiar_pantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def mostrar_menu():
-    print("1. Agregar Tarea")
-    print("2. Mostrar Tareas")
-    print("3. Salir")
+    print("1. Agregar Tarea Simple")
+    print("2. Agregar Tarea Recurrente")
+    print("3. Mostrar Tareas")
+    print("4. Salir")
     print('======================================================')
-def agregar_tarea(gestion):
+
+def agregar_tarea_simple(gestion):
     id = input("Ingrese el ID de la tarea: ")
     titulo = input("Ingrese el título de la tarea: ")
     descripcion = input("Ingrese la descripción de la tarea: ")
     fecha_ingreso = input("Ingrese la fecha de ingreso (YYYY-MM-DD): ")
     fecha_vencimiento = input("Ingrese la fecha de vencimiento (YYYY-MM-DD): ")
     estado = input("Ingese el estado, 1. Pendiente, 2. En Progreso, 3. Completada")
-    tarea = Tarea(id, titulo, descripcion, fecha_ingreso, fecha_vencimiento, estado)
+    tarea = TareaSimple(id, titulo, descripcion, fecha_ingreso, estado, fecha_vencimiento)
+
+    gestion.agregar_tarea(tarea)
+    input("Presione Enter para continuar...")
+
+def agregar_tarea_recurrente(gestion):
+    id = input("Ingrese el ID de la tarea: ")
+    titulo = input("Ingrese el título de la tarea: ")
+    descripcion = input("Ingrese la descripción de la tarea: ")
+    fecha_ingreso = input("Ingrese la fecha de ingreso (YYYY-MM-DD): ")
+    frecuencia = input("Ingrese la frecuencia de la tarea (diaria, semanal, mensual): ")
+    estado = input("Ingese el estado, 1. Pendiente, 2. En Progreso, 3. Completada")
+    tarea = TareaRecurrente(id, titulo, descripcion, fecha_ingreso, estado, frecuencia)
 
     gestion.agregar_tarea(tarea)
     input("Presione Enter para continuar...")
@@ -35,9 +50,11 @@ if __name__ == '__main__':
         mostrar_menu()
         opcion = input("Seleccione una opción: ")
         if opcion == '1':
-            agregar_tarea(gestion)
+            agregar_tarea_simple(gestion)
         elif opcion == '2':
+            agregar_tarea_recurrente(gestion)
+        elif opcion == '3':
             gestion.mostrar_tareas()
             input("Presione Enter para continuar...")
-        elif opcion == '3':
+        elif opcion == '4':
             sys.exit()
--- tareas.py
+++ tareas.py
@@ -2,12 +2,11 @@ import datetime
 import json
 
 class Tarea:
-    def __init__(self, id, titulo, descripcion, fecha_ingreso, fecha_vencimiento, estado):
+    def __init__(self, id, titulo, descripcion, fecha_ingreso, estado):
         self.id = self.validar_id(id)
         self.titulo = self.validar_titulo(titulo)
         self.descripcion = descripcion
         self.fecha_ingreso = fecha_ingreso
-        self.fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
         self.estado = self.validar_estado(estado)
 
     def __str__(self):
@@ -26,18 +25,12 @@ class Tarea:
     def fecha_ingreso(self):
         return self.__fecha_ingreso
     @property
-    def fecha_vencimiento(self):
-        return self.__fecha_vencimiento
-    @property
     def estado(self):
         return self.__estado
     @fecha_ingreso.setter
     def fecha_ingreso(self, fecha_ingreso):
         self.__fecha_ingreso = fecha_ingreso
-    @fecha_vencimiento.setter
-    def fecha_vencimiento(self, fecha_vencimiento):
-        self.__fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
-        return self.__fecha_vencimiento
+
     @estado.setter
     def estado(self, estado):
         self.__estado = self.validar_estado(estado)
@@ -63,14 +56,6 @@ class Tarea:
         if not isinstance(int(id), int) or int(id) <= 0:
             raise ValueError("El ID debe ser un número entero positivo.")
         return id
-
-    def validar_fecha_vencimiento(self, fecha_vencimiento):
-        try:
-            if fecha_vencimiento < datetime.datetime.now().strftime('%Y-%m-%d'):
-                raise ValueError("La fecha de vencimiento no puede ser en el pasado.")
-        except ValueError as e:
-            raise ValueError(f"Fecha de vencimiento inválida: {e}")
-        return fecha_vencimiento
     
     def validar_estado(self, estado):
         if estado not in ["pendiente", "en progreso", "completada"]:
@@ -83,10 +68,65 @@ class Tarea:
             "titulo": self.__titulo,
             "descripcion": self.__descripcion,
             "fecha_ingreso": self.__fecha_ingreso,
-            "fecha_vencimiento": self.__fecha_vencimiento,
             "estado": self.__estado
         }
 
+class TareaSimple(Tarea):
+    def __init__(self, id, titulo, descripcion, fecha_ingreso, estado, fecha_vencimiento):
+        super().__init__(id, titulo, descripcion, fecha_ingreso, estado)
+        self.fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
+
+    @property
+    def fecha_vencimiento(self):
+        return self.__fecha_vencimiento
+    @fecha_vencimiento.setter
+
+    def fecha_vencimiento(self, fecha_vencimiento):
+        self.__fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
+        return self.__fecha_vencimiento
+    
+    def validar_fecha_vencimiento(self, fecha_vencimiento):
+        try:
+            if fecha_vencimiento < datetime.datetime.now().strftime('%Y-%m-%d'):
+                raise ValueError("La fecha de vencimiento no puede ser en el pasado.")
+        except ValueError as e:
+            raise ValueError(f"Fecha de vencimiento inválida: {e}")
+        return fecha_vencimiento
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["fecha_vencimiento"] = self.fecha_vencimiento
+        return data
+    
+    def __str__(self):
+        return f"super().__str__() - Vencimiento: {self.fecha_vencimiento}" 
+
+class TareaRecurrente(Tarea):
+    def __init__(self, id, titulo, descripcion, fecha_ingreso, estado, frecuencia):
+        super().__init__(id, titulo, descripcion, fecha_ingreso, estado)
+        self.frecuencia = self.validar_frecuencia(frecuencia)
+    
+    @property
+    def frecuencia(self):
+        return self.__frecuencia
+    
+    @frecuencia.setter
+    def frecuencia(self, frecuencia):
+        self.__frecuencia = self.validar_frecuencia(frecuencia)
+        return self.__frecuencia
+    
+    def validar_frecuencia(self, frecuencia):
+        if frecuencia not in ["diaria", "semanal", "mensual"]:
+            raise ValueError("Frecuencia inválida. Debe ser 'diaria', 'semanal' o 'mensual'.")
+        return frecuencia
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["frecuencia"] = self.frecuencia
+        return data
+    
+    def __str__(self):
+        return f"super().__str__() - Frecuencia: {self.frecuencia}"
 class GestionTareas:
     def __init__(self, archivo):
         self.archivo = archivo
@@ -121,7 +161,11 @@ class GestionTareas:
             print("No hay tareas registradas.")
         else:
             for tarea in datos.values():
-                print(tarea['id'] + ' ' + tarea['titulo'] + ' ' + tarea['descripcion'] + ' ' + tarea['fecha_ingreso'] + ' ' + tarea['fecha_vencimiento'] + ' ' + tarea['estado'])
+                if 'fecha_vencimiento' in tarea:
+                    print(tarea['id'] + ' ' + tarea['titulo'] + ' ' + tarea['descripcion'] + ' ' + tarea['fecha_ingreso'] + ' ' + tarea['fecha_vencimiento'] + ' ' + tarea['estado'])
+                else:
+                    print(tarea['id'] + ' ' + tarea['titulo'] + ' ' + tarea['descripcion'] + ' ' + tarea['fecha_ingreso'] + ' ' + tarea['frecuencia'] + ' ' + tarea['estado'])
+    
         print("-----------------------------------------------------------------------------")
 
     def eliminar_tarea(self, descripcion):
